f0d0d53dfe3e940950d25bd6046aa7c974e12687
aka <irunwazed@gmail.com>
2025-09-02 10:01:49 +0700

    perbaikan

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,16 +1,17 @@
 import { defineConfig } from "astro/config";
-
+import node from '@astrojs/node';
 import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
   server: {
-    host: '0.0.0.0',
-    port: 3000
+    port: 3000,
+    host: "0.0.0.0"
   },
-  experimental: {
-    staticImportMetaEnv: true,
-  },
+  output: 'server',
+  adapter: node({
+    mode: 'standalone',
+  }),
   vite: {
     plugins: [tailwindcss()]
   }
